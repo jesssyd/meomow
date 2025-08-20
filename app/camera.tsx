acf68597396ceab0a1b76908b71e54fd50ddd8b3
jessica <tidd.jessica.s@gmail.com>
2025-08-20 17:15:57 -0400

    Updated camera.tsx

--- app/camera.tsx
+++ app/camera.tsx
@@ -11,7 +11,7 @@ import {
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { X, SwitchCamera Zap, ZapOff, Plus, Minus } from 'lucide-react-native';
+import { X, SwitchCamera, Zap, ZapOff, Plus, Minus } from 'lucide-react-native';
 import { Colors } from '@/constants/Colors';
 
 const { width } = Dimensions.get('window');
