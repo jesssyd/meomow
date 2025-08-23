13bfbfdef06a8b2d158b162d50c7eadcba7b2d01
jessica <tidd.jessica.s@gmail.com>
2025-08-23 16:41:53 -0400

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,7 +36,7 @@ export default function TabLayout() {
             <Home 
               size={size} 
               color={color} 
-              strokeWidth={2.5}
+              strokeWidth={2}
             />
           ),
         }}
@@ -49,7 +49,7 @@ export default function TabLayout() {
             <CirclePlus
               size={size + 4} 
               color={color} 
-              strokeWidth={2.5}
+              strokeWidth={2}
             />
           ),
         }}
@@ -68,7 +68,7 @@ export default function TabLayout() {
             <CircleUserRound 
               size={size} 
               color={color} 
-              strokeWidth={2.5}
+              strokeWidth={2}
             />
           ),
         }}
