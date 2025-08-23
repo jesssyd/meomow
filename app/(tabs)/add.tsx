b04ec851ed54ba1cf19c96c0bad4e94546fd6b3a
jessica <tidd.jessica.s@gmail.com>
2025-08-23 18:17:51 -0400

    Updated add.tsx

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function AddPlaceholderScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>this screen redirects to add-cat</Text>
+      <Text>redirecting to add cat</Text>
     </View>
   );
 }
