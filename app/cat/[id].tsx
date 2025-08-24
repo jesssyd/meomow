60ff1de9b9dadde70661e4e5937cd1f81e7f2ea8
jessica <tidd.jessica.s@gmail.com>
2025-08-24 01:14:22 -0400

    Updated [id].tsx

--- app/cat/[id].tsx
+++ app/cat/[id].tsx
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: Colors.primary.backgroundAlt,
+    backgroundColor: '',
   },
   loading: {
     fontFamily: 'Jua-Regular',
